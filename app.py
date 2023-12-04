6347cdc93a609631a1804aabea1a402e21fd8208
UgoBlent <151767546+UgoBlent@users.noreply.github.com>
2023-12-04 14:17:25 +0100

    Update app.py
    Mise en place des permiers URL afin de requêter notre API.

--- app.py
+++ app.py
@@ -1,29 +1,58 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+import json
+
+from flask import Flask, jsonify
+from flask import request
+import psycopg2
 
 app = Flask(__name__)
 
+def listify(vals):
+    return [elem for val in vals for elem in val]
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@10.19.2.1:5432/projet_S7_fil_actu'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+def execute_query(query):
+    connex = psycopg2.connect(host="10.19.2.1", database="projet_actu", user="actu_user", password="ValMaxMatAma")
+    cursor = connex.cursor()
+    cursor.execute(query)
+    vals = cursor.fetchall()
+    cursor.close()
+    connex.close()
+    return vals
 
-# Simuler une base de données avec une liste en mémoire
-news_db = [
-    {"id": 1, "title": "Nouvelle importante", "content": "Contenu de la nouvelle importante"}
-]
+def execute_insert(query):
+    connex = psycopg2.connect(host="10.19.2.1", database="projet_actu", user="actu_user", password="ValMaxMatAma")
+    cursor = connex.cursor()
+    try:
+        cursor.execute(query)
+        connex.commit()
+    except Exception as e:
+        print("Une erreur est survenue:", e)
+        connex.rollback()
+    finally:
+        cursor.close()
+        connex.close()
 
-@app.route('/news', methods=['GET'])
-def get_news():
-    """Endpoint pour récupérer les actualités."""
-    return Flask(news_db)
+
+@app.route('/groupe', methods=['GET'])
+def get_groupe():
+    result = execute_query('SELECT * FROM "groupe";')
+    return jsonify(result)
+
+@app.route('/actu', methods=['GET'])
+def get_actu():
+    result = execute_query('SELECT * FROM "actu";')
+    return jsonify(result)
+
+
 
 @app.route('/news', methods=['POST'])
-def post_news():
-    """Endpoint pour publier une nouvelle actualité."""
-    news_data = Flask.json
-    news_data['id'] = len(news_db) + 1  # Assigner un ID simple
-    news_db.append(news_data)
-    return Flask(news_data), 201
+def create_groupe():
+    data = request.json  # Supposons que les données sont envoyées au format JSON
+    # Vous devez valider et structurer 'data' en fonction du format de votre table
+
+    query = 'INSERT INTO "groupe" (nom) VALUES ("' + data['nom'] + '");'
+
+    return jsonify({"message": "Groupe créé avec succès"}), 201
+
 
 if __name__ == '__main__':
     app.run(debug=True)
